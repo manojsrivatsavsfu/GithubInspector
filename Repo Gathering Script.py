5715b9e0ca7680c87d7f780fae118d59d0342052
justin-go123 <go.justinjude@gmail.com>
2025-03-24 20:34:17 -0700

    Github Repo Data Gatherer

--- Repo Gathering Script.py
+++ Repo Gathering Script.py
@@ -11,6 +11,7 @@ dumpDir = "Dump-" + str(datetime.now().time())
 
 os.chdir(pwd)
 
+#open JSON file from Github Search API
 with open(pwd + '/repositories1.json', 'r') as file:
     data = json.load(file)
 
@@ -18,18 +19,20 @@ os.mkdir(dumpDir)
 os.chdir(pwd + "/" + dumpDir)
 pwd = os.getcwd()
 
-
+#Parse through each object
 for repo in data['items']:
     
     print("Repo: " + repo['name'])
     fullRepo = repo['html_url']
     repo = fullRepo.split('/')[-1]
+    #clones the repo
     os.system("git clone --depth 1 " + fullRepo)
+    #checks if the workflow folder exists
     existing = os.path.exists(pwd + "/" + repo + '/.github/workflows')
-
+    #keeps folder if it exists
     if existing == True:
         print("YAML folder exists")
-        
+    #removes the directory if it doesn't
     else:
         shutil.rmtree(pwd + "/" + repo)
 
