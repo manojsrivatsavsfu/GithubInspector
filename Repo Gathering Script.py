403e44975f232ce3c56d4c0bcd46d8f928f70be4
justin-go123 <go.justinjude@gmail.com>
2025-04-06 03:47:12 -0700

    Add files via upload

--- Repo Gathering Script.py
+++ Repo Gathering Script.py
@@ -3,20 +3,25 @@ import shutil
 import time
 import json
 from datetime import datetime
+import urllib
+import urllib.request
 
 jsondata = []
 
-pwd = os.getcwd()
+url = 'https://api.github.com/search/repositories?q=created:2024-01-01&sort=stars&order=desc&page=1&per_page=100'
+apiData = urllib.request.urlopen(url)
+
+
+
+pwdStart = os.getcwd()
 dumpDir = "Dump-" + str(datetime.now().time())
 
-os.chdir(pwd)
+os.chdir(pwdStart)
 
-#open JSON file from Github Search API
-with open(pwd + '/repositories1.json', 'r') as file:
-    data = json.load(file)
+data = json.load(apiData)
 
 os.mkdir(dumpDir)
-os.chdir(pwd + "/" + dumpDir)
+os.chdir(pwdStart + "/" + dumpDir)
 pwd = os.getcwd()
 
 #Parse through each object
@@ -28,13 +33,28 @@ for repo in data['items']:
     #clones the repo
     os.system("git clone --depth 1 " + fullRepo)
     #checks if the workflow folder exists
-    existing = os.path.exists(pwd + "/" + repo + '/.github/workflows')
+    GHexisting = os.path.exists(pwd + "/" + repo + '/.github/workflows')
+    consolidatedExisting = os.path.exists(pwdStart + "/" + "consolidated")
+    
+    if consolidatedExisting == False:
+        os.mkdir(pwdStart + "/" + "consolidated")
+
     #keeps folder if it exists
-    if existing == True:
+    if GHexisting == True:
         print("YAML folder exists")
-    #removes the directory if it doesn't
+        consolidated = pwdStart + "/" + "consolidated" + "/" + repo
+        os.mkdir(consolidated)
+
+        for file in os.listdir(pwd + "/" + repo + '/.github/workflows'):
+            if file.endswith(".yml") or file.endswith(".yaml"):
+                shutil.copy(pwd + "/" + repo + '/.github/workflows' + '/' + file, consolidated)
+        shutil.rmtree(pwd + "/" + repo)
+
+    #removes the directory if it doesn't have github actions yaml files
     else:
         shutil.rmtree(pwd + "/" + repo)
+
+
 
     print("============================")
     time.sleep(5)
